bb92215f023a97d1e5080c65b0c51fa2721b55d9
MengTL4 <53808347+MengTL4@users.noreply.github.com>
2023-03-17 09:35:19 +0800

    Update Task.go

--- Request/Task.go
+++ Request/Task.go
@@ -3,13 +3,19 @@ package Request
 import "Daxuexi/Config"
 
 func Task() {
-	cookie, authorization, member_id := Config.ReadConfig()
+	cookie, authorization, member_id, identity := Config.ReadConfig()
 	lessonid := GetLesson(cookie)
 	pngid := DoLesson(lessonid, cookie)
 	Download(pngid)
 	XCosSecurityToken, Authorization := GetAuthentication(authorization)
 	key := generateRandomString(4)
 	filepath := SubmitPng(key, XCosSecurityToken, Authorization)
-	id, create_at, invest := GetInfo(authorization, member_id)
-	SubmitData(id, create_at, filepath, invest, member_id, authorization)
+	id, create_at := GetInfo(authorization, member_id)
+	if identity == "student" {
+		investid, subjectid, itemDetailstudentid1, itemDetailstudentid2 := GetInfoStudent(authorization, id)
+		SubmitData(id, create_at, filepath, member_id, authorization, investid, subjectid, itemDetailstudentid1, itemDetailstudentid2)
+	} else {
+		answerid, subjecteacherid, itemDetailsteacherid1, itemDetailsteacherid2 := GetInfoTeacher(authorization, id)
+		SubmitData(id, create_at, filepath, member_id, authorization, answerid, subjecteacherid, itemDetailsteacherid1, itemDetailsteacherid2)
+	}
 }
